4fbb40182598cbbcafabe3989c3a223a038ae9ae
Gigabitzauber <gigabitzauber@posteo.de>
2026-06-27 17:30:15 +0200

    JcExceptionTest

--- src/main/java/de/mosig/gigabitzauber/jancontrol/error/JcException.java
+++ src/main/java/de/mosig/gigabitzauber/jancontrol/error/JcException.java
@@ -1,11 +1,18 @@
 package de.mosig.gigabitzauber.jancontrol.error;
 
+import java.util.Objects;
+
 public final class JcException extends RuntimeException {
-    public JcException(String msg) {
-        super(msg);
+    public JcException() {
+        super("Unexpected error");
     }
-    
+
+    public JcException(String msg) {
+        super(Objects.requireNonNull(msg, "message must not be null"));
+    }
+
     public JcException(String msg, final Throwable cause) {
-        super(msg, cause);
+        super(Objects.requireNonNull(msg, "message must not be null"),
+            Objects.requireNonNull(cause, "cause must not be null"));
     }
 }
