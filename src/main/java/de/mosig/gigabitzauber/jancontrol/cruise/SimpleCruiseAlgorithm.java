ea2725397c852120f7c7d9e61149fde866b84431
Gigabitzauber <gigabitzauber@posteo.de>
2026-07-07 17:27:11 +0200

    SimpleCruiseAlgorithmTest

--- src/main/java/de/mosig/gigabitzauber/jancontrol/cruise/SimpleCruiseAlgorithm.java
+++ src/main/java/de/mosig/gigabitzauber/jancontrol/cruise/SimpleCruiseAlgorithm.java
@@ -1,19 +1,24 @@
 package de.mosig.gigabitzauber.jancontrol.cruise;
 
+import com.google.common.collect.Range;
 import de.mosig.gigabitzauber.jancontrol.domain.Fan;
 import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
+import static java.util.Objects.requireNonNull;
+
 public final class SimpleCruiseAlgorithm implements Runnable {
+
+    private static final Range<Integer> VALID_WRITE_RANGE = Range.closed(20, 100);
 
     private final Fan fan;
     private final Logger log;
 
     public SimpleCruiseAlgorithm(Fan fan, Logger log) {
-        this.fan = fan;
-        this.log = log;
+        this.fan = requireNonNull(fan, "fan must not be null");
+        this.log = requireNonNull(log, "log must not be null");
     }
 
     @Override
@@ -37,13 +42,30 @@ public final class SimpleCruiseAlgorithm implements Runnable {
             log.info("Cruise command got interrupted. Shutting down..");
         } else {
             var newRpm = Collections.max(rpmCandidates);
-            var targetRpmValue = newRpm.targetRpm;
-            if (targetRpmValue < 20) {
-                targetRpmValue = 20;
-            }
+            var targetRpmValue = safeGetTargetRpm(newRpm);
             fan.device().write(targetRpmValue);
             log.debug(newRpm.toString());
         }
+    }
+
+    private int safeGetTargetRpm(RpmCandidate newRpm) {
+        var targetRpmValue = newRpm.targetRpm;
+        if (!VALID_WRITE_RANGE.contains(targetRpmValue)) {
+            log.warn("Calculated RPM value for {} exceeds safe limits.", newRpm.targetDeviceName);
+            var lowestSafeRpmValue = VALID_WRITE_RANGE.lowerEndpoint();
+            if (targetRpmValue < lowestSafeRpmValue) {
+                targetRpmValue = lowestSafeRpmValue;
+                log.warn("Setting RPM value for {} to lowest allowed value: {}", newRpm.targetDeviceName, lowestSafeRpmValue);
+            } else {
+                var highestSafeRpmValue = VALID_WRITE_RANGE.upperEndpoint();
+                if (targetRpmValue > highestSafeRpmValue) {
+                    targetRpmValue = highestSafeRpmValue;
+                    log.warn("Setting RPM value for {} to highest allowed value: {}", newRpm.targetDeviceName, highestSafeRpmValue);
+                }
+            }
+        }
+
+        return targetRpmValue;
     }
 
     private static record RpmCandidate(
